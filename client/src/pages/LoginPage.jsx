bdb808653dab29ac33cd6e8670d28edfa0d0ec56
Karthik P <padiyil2004@gmail.com>
2025-10-01 15:51:41 +0530

    completed-frontend-started-backend

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,17 +3,60 @@ import assets from '../assets/assets'
 
 const LoginPage = () => {
   const [currentState,setCurrentState] = useState('Sign Up');
+  const [fullName,setfullName] = useState('');
+  const [email,setemail] = useState('');
+  const [bio,setbio] = useState('');
+  const [password,setPassword] = useState('');
+  const [isDataSubmitted,setisDataSubmitted] = useState(false);
+
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if(currentState==='Sign Up' && !isDataSubmitted)
+    {
+      setisDataSubmitted(true);
+      return ;
+    }
+  }
+
   return (
     <div className='min-h-screen backdrop-blur-2xl bg-cover bg-center flex items-center justify-center sm:justify-evenly max-sm:flex-col'>
       {/* -------------------------- */}
-      <img src={assets.logo_big} className='w-[min(30vm,250px)]'/>
+      <img src={assets.logo_big} className='w-60'/>
       {/* -------------------------- */}
-      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+      <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-4 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex justify-between items-center'>
           {currentState}
-          <img src={assets.arrow_icon} className='cursor-pointer w-5'/>
+          {isDataSubmitted && <img onClick={() => setisDataSubmitted(false)} src={assets.arrow_icon} className='cursor-pointer w-5'/>}
         </h2>
-        
+        {currentState === "Sign Up" && !isDataSubmitted && <input onChange={(e) => setfullName(e.target.value)} value={fullName} type='text' className='p-2 border border-gray-500 rounded-md focus:ring-2 focus:ring-indigo-500' placeholder='Enter Full name' required/>}
+        {!isDataSubmitted && (
+          <>
+            <input onChange={(e) => setemail(e.target.value)} value={email} type='email' className='p-2 border border-gray-500 rounded-md focus:ring-2 focus:ring-indigo-500' placeholder='Enter Email' required/>
+            <input onChange={(e) => setPassword(e.target.value)} value={password} type='password' className='p-2 border border-gray-500 rounded-md focus:ring-2 focus:ring-indigo-500 ' placeholder='Enter Password' required/>
+          </>
+        )}
+        {currentState ==='Sign Up' && isDataSubmitted && (
+          <textarea onChange={(e) => setbio(e.target.value)} value={bio} row={4} className='p-2 border border-gray-500 rounded-md focus:ring-2 focus:ring-indigo-500 ' placeholder='Set a short Bio....' required>
+          </textarea>
+        )}
+        <button type='submit' className='bg-gradient-to-r from-purple-500 to-violet-600 rounded-full py-3 cursor-pointer'>
+          {currentState === 'Sign Up' ? "Create Account" : "Login Now"}
+        </button>
+        {currentState === 'Sign Up' && (
+          <div className='flex items-center gap-2 text-sm text-gray-500'>
+            <input type='checkbox'/>
+            <p> Agree to terms of use & privacy policy.</p>
+          </div>
+        )}
+        <div className='flex flex-col text-gray-500 gap-2'>
+          {currentState === 'Sign Up' ? (
+            <p className='text-sm'>Already have an account? <span onClick={() => {setCurrentState('Login');setisDataSubmitted(false)}} className=' cursor-pointer text-indigo-500 font-medium'>Login Here</span></p>
+          ):(
+            <p className='text-sm'>Create an account? <span onClick={() => {setCurrentState('Sign Up');setisDataSubmitted(false)}}className=' cursor-pointer text-indigo-500 font-medium'>Click Here</span></p>
+          )}
+        </div>
+
       </form>
     </div>
   )
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import assets from '../assets/assets';
 
 const ProfilePage = () => {
+
+  const [selectedImg,setSelectedImg] = useState(null);
+  const navigate = useNavigate();
+  const [name,setName] = useState("Karthi P");
+  const [bio,setBio] = useState("Hei everyone");
+
   return (
-    <div>
-      
+    <div className='min-h-screen flex justify-center items-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-white border-2 border-gray-600 flex justify-between items-center rounded-lg
+      max-sm:flex-col-reverse'>
+        <form className='flex flex-col p-10 gap-5 flex-1'>
+          <h3 className='text-lg'>Profile Details</h3>
+          <label htmlFor='avatar' className='flex items-center gap-3 cursor-pointer'>
+            <input onChange={(e) => setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg'  hidden/>
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}/> Upload Profile Pic
+          </label>
+          <input type='text' onChange={(e) => setName(e.target.value)} placeholder='Your Name'  value={name} required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' />
+          <textarea type='te' onChange={(e) => setBio(e.target.value)} placeholder='Enter the Bio'  value={bio} required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500 text-white' rows={4} />
+          <button type='submit' className='bg-gradient-to-r from-violet-500 to-indigo-600 text-white rounded-full p-2 cursor-pointer'>
+            Save
+          </button>
+        </form>
+        <img src={assets.logo_icon} className='max-w-40 aspect-square rounded-full mx-10 max-sm:mt-10' />
+      </div>
     </div>
   )
-}
+} 
 
 export default ProfilePage
