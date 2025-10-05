e6a2d01c6a64e06de2d79eb770c553dc5c80c3eb
Karthik P <padiyil2004@gmail.com>
2025-10-05 21:48:02 +0530

    changed logout function for None of the cookie

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -132,7 +132,7 @@ export const logout = async(req,res) => {
     res.clearCookie("token",{
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
     })
     res.status(200).json({
       success: true,
