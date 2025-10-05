ee5da23e19aad03db2e3159a660d8bbd5b341ccb
Karthik P <padiyil2004@gmail.com>
2025-10-05 21:36:38 +0530

    made cookies samesite to none

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -30,7 +30,7 @@ export const signup = async(req,res) => {
     res.cookie("token", token, {
         httpOnly: true, 
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict", 
+        sameSite: "None", 
         maxAge: 7 * 24 * 60 * 60 * 1000 
     })
     res.status(201).json({
@@ -77,7 +77,7 @@ export const login = async(req,res) => {
     res.cookie("token", token, {
         httpOnly: true, 
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict", 
+        sameSite: "None", 
         maxAge: 7 * 24 * 60 * 60 * 1000 
     })
     return res.status(200).json({
